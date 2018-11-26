1b0760d8a38dd47347de1d0c9dc4cce502f2cec0
fans <ronfan92131@gmail.com>
2018-11-26 06:32:13 -0800

    AddNoteActivity

---
+++ app/src/main/java/com/doyen/fans/mvvm/AddNoteActivity.java
@@ -0,0 +1,45 @@
+package com.doyen.fans.mvvm;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+
+import com.doyen.fans.mvvm.R;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private NumberPicker numberPickerPriority;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        editTextTitle = findViewById(R.id.text_view_title);
+        editTextDescription = findViewById(R.id.text_view_description);
+        numberPickerPriority = findViewById(R.id.number_picker_priority);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
